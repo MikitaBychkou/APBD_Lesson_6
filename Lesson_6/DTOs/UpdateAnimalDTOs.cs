d00043f64afeeab73f120a47525fcf8c7340fe79
Mikita Bychkou <nikitoscoin@gmail.com>
2024-04-24 18:52:24 +0200

    second

--- Lesson_6/DTOs/UpdateAnimalDTOs.cs
+++ Lesson_6/DTOs/UpdateAnimalDTOs.cs
@@ -6,4 +6,4 @@ public record AnimalUpdateDto(
     [MaxLength(200)] string Name,
     [Required] [MaxLength(200)] string Description,
     [MaxLength(200)] string Category,
-    [MaxLength(200)] string Area);
+    [MaxLength(200)] string Area) ;
