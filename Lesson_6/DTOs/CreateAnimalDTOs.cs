dbf3d02554c295246390b17af169f2ad8b05f998
Mikita Bychkou <nikitoscoin@gmail.com>
2024-04-29 14:38:08 +0200

    Second commit

--- Lesson_6/DTOs/CreateAnimalDTOs.cs
+++ Lesson_6/DTOs/CreateAnimalDTOs.cs
@@ -6,4 +6,4 @@ public record AnimalCreateDto(
     [Required] [MaxLength(200)] string Name, 
     [Required] [MaxLength(200)] string Description, 
     [Required] [MaxLength(200)] string Category, 
-    [Required] [MaxLength(200)] string Area);
+    [Required] [MaxLength(200)] string Area );
